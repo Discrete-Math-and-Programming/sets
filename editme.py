98127d2d611866e02356522ee7686b3b85b2ea90
Umut Özge <umut.oezge@gmail.com>
2025-02-15 17:33:58 +0300

    test fixed

--- editme.py
+++ editme.py
@@ -44,20 +44,20 @@ def test():
             return "[Error]"
 
     test_cases = {
-        "set_from_list": (set_from_list, [[1, 2, 3]], {1: None, 2: None, 3: None}),
-        "list_from_set": (list_from_set, [{1: None, 2: None, 3: None}], [1, 2, 3]),
-        "add": (add, [{1: None, 2: None}, 3], {1: None, 2: None, 3: None}),
-        "remove": (remove, [{1: None, 2: None, 3: None}, 2], {1: None, 3: None}),
-        "union": (union, [{1: None, 2: None}, {2: None, 3: None}], {1: None, 2: None, 3: None}),
-        "intersection": (intersection, [{1: None, 2: None}, {2: None, 3: None}], {2: None}),
-        "difference": (difference, [{1: None, 2: None}, {2: None, 3: None}], {1: None}),
-        "symmetric_difference": (symmetric_difference, [{1: None, 2: None}, {2: None, 3: None}], {1: None, 3: None}),
-        "is_subset_of": (is_subset_of, [{1: None, 2: None}, {1: None, 2: None, 3: None}], True),
-        "is_superset_of": (is_superset_of, [{1: None, 2: None, 3: None}, {1: None, 2: None}], True),
+        "set_from_list": ([[1, 2, 3]], {1: None, 2: None, 3: None}),
+        "list_from_set": ([{1: None, 2: None, 3: None}], [1, 2, 3]),
+        "add": ([{1: None, 2: None}, 3], {1: None, 2: None, 3: None}),
+        "remove": ([{1: None, 2: None, 3: None}, 2], {1: None, 3: None}),
+        "union": ([{1: None, 2: None}, {2: None, 3: None}], {1: None, 2: None, 3: None}),
+        "intersection": ([{1: None, 2: None}, {2: None, 3: None}], {2: None}),
+        "difference": ([{1: None, 2: None}, {2: None, 3: None}], {1: None}),
+        "symmetric_difference": ([{1: None, 2: None}, {2: None, 3: None}], {1: None, 3: None}),
+        "is_subset_of": ([{1: None, 2: None}, {1: None, 2: None, 3: None}], True),
+        "is_superset_of": ([{1: None, 2: None, 3: None}, {1: None, 2: None}], True),
     }
     
     print("Function Name            | Status")
     print("-------------------------|--------")
-    for name, (func, args, expected) in test_cases.items():
-        status = test_function(func, *args, expected=expected)
+    for name, (args, expected) in test_cases.items():
+        status = test_function(eval(name), *args, expected=expected) if name in globals() else "[Not Implemented]"
         print(f"{name:<25} | {status}")
